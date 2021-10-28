6f77149c126a3496e52f66d428dff65775993fde
piegames <git@piegames.de>
2021-10-28 22:35:44 +0200

    Make it stateful

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::rc::Rc;
-use hubcaps::{Github};
-use futures::stream::{StreamExt, TryStreamExt};
+use std::collections::{BTreeMap, HashMap};
+use hubcaps::{Github, SortDirection, issues::{State, Sort}, pulls::PullListOptions};
+use futures::stream::{StreamExt};
 use anyhow::Context;
 
 mod lib;
 use lib::*;
+
+// (pr -> (owner -> [files that matched]))
+type PersistentState = BTreeMap<u64, HashMap<String, Vec<String>>>;
 
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
@@ -12,6 +16,9 @@ pub async fn main() -> anyhow::Result<()> {
         .with_level(log::LevelFilter::Info)
         .init()
         .context("Failed to initialize logger")?;
+
+    let mut state: PersistentState = serde_json::from_reader(std::fs::File::open("state.json")?)?;
+
     let github = Github::new(
         concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")),
         None,
@@ -23,27 +30,39 @@ pub async fn main() -> anyhow::Result<()> {
     ).unwrap());
 
     let pulls = repo.pulls();
-    let stream = futures::stream::iter(
-        pulls
-            .list(&Default::default())
-            .await?
-            .into_iter()
-    );
-    stream
-        .inspect(|pull| log::info!("Processing PR: {}", pull.number))
-        .map(|pull| (pull.number, pulls.get(pull.number)))
-        .flat_map(|(number, pr)| pr.iter_files().map_ok(move |file| (number, file)))
-        .map_ok(|(number, diff)| (number, format!("/{}", diff.filename)))
-        .try_for_each(|(number, path)| {
-            let codeowners = codeowners.clone();
-            async move {
-                for owner in codeowners.matches(&path).into_iter().flatten() {
-                    log::info!("PR {}: Ping {}, because of '{}'", number, owner, path);
+    let mut pulls_stream = pulls
+        .iter(&{
+            let mut builder = PullListOptions::builder();
+            builder.state(State::Open);
+            builder.sort(Sort::Updated);
+            builder.direction(SortDirection::Desc);
+            builder.build()
+        });
+    while let Some(pull) = pulls_stream.next().await.transpose()? {
+        let number = pull.number;
+        if state.contains_key(&number) {
+            log::info!("(Skipping #{})", number);
+            continue;
+        }
+        log::info!("Processing PR: #{}", number);
+        
+        let pr = pulls.get(number);
+        let mut iter = pr.iter_files();
+        let mut pinged = HashMap::<String, Vec<String>>::new();
+        while let Some(diff) = iter.next().await.transpose()? {
+            let path = format!("/{}", diff.filename);
+            for owner in codeowners.matches(&path).into_iter().flatten() {
+                log::info!("PR {}: Ping {}, because of '{}'", number, owner, path);
+                match pinged.entry(owner.to_string()) {
+                    std::collections::hash_map::Entry::Occupied(mut e) => e.get_mut().push(path.clone()),
+                    std::collections::hash_map::Entry::Vacant(e) => {e.insert(vec![path.clone()]);},
                 }
-                Ok(())
             }
-        })
-        .await?;
+        }
+        state.insert(number, pinged);
 
+        serde_json::to_writer_pretty(std::fs::File::create("state.json")?, &state)?;
+    }
+    
     Ok(())
 }
